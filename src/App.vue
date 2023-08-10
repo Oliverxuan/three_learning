a8f67f915c159294fde13de9194cb3f636dfc736
Oliver <2373734127@qq.com>
2023-08-11 01:17:30 +0800

    feat:【模型加载】

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,66 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class='container'></div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script setup lang='ts'>
+import * as THREE from "three"
+import  {GLTFLoader}  from 'three/examples/jsm/loaders/GLTFLoader.js' 
+import TWEEN from "@tweenjs/tween.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
+import { reactive, ref, onMounted } from 'vue'
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  45,//视角
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: "#ffff00" })
+const cube = new THREE.Mesh(geometry, material)
+// scene.add(cube)
+camera.position.z = 15
+camera.lookAt(0, 0, 0)
+// ---------------------------------------------------------------------
+// Axes
+// ---------------------------------------------------------------------
+var ambientLight = new THREE.AmbientLight("white");
+ambientLight.position.set(100, 150, 135); 
+scene.add(ambientLight);
+
+const loop = () => {
+  requestAnimationFrame(loop)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+  controls.update(); 
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+const loader = new GLTFLoader();
+loader.load('/model.gltf', function (gltf:any) {
+    const model = gltf.scene;
+    scene.add(model);
+    console.log("success");
+    
+}, undefined, function (error:any) {
+    console.error(error);
+});
+const controls = new OrbitControls(camera, renderer.domElement);
+onMounted(() => {
+  loop()
+})
+</script>
+<style  lang='scss' scoped>
+.container {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  height: 100%;
 }
 </style>
